a30df07b8bc689fac1b2762c3cf4ae8fe5381253
Alphax978 <jamesrawal9191@gmail.com>
2025-01-23 17:34:25 +0545

    initial website layout

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const app_name = process.env.NEXT_PUBLIC_APP_NAME || "nepzone2"
+export const app_slogan = process.env.NEXT_PUBLIC_APP_SLOGAN || "spend less, enjoy more"
+export const app_description = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "shop with no regrets"
