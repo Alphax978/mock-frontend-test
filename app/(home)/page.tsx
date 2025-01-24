d8c9ea25ac356966d35efac6f1d86eadd393ad5a
Alphax978 <jamesrawal9191@gmail.com>
2025-01-24 12:31:40 +0545

    product-todaydeal-secton

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,10 @@
 import { HomeCard } from '@/components/shared/home/homecard'
 import HomeCarousel  from '@/components/shared/home/home-carousel'
-import { getAllCategories, getProductsForCard } from '@/lib/actions/product.actions'
+import { getAllCategories, getProductsForCard, getProductsByTag } from '@/lib/actions/product.actions'
 import { toSlug } from '@/lib/utils'
 import data from '@/lib/data'
+import { Card, CardContent } from '@/components/ui/card'
+import ProductSlider from '@/components/shared/product/product-slider'
 
 export default async function HomePage() {
   const {carousels} = data
@@ -58,11 +60,19 @@ export default async function HomePage() {
     },
   ]
 
+  const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
+
   return (
     <>
+
       <HomeCarousel items={carousels} />
       <div className='md:p-4 md:space-y-4 bg-border'>
         <HomeCard cards={cards} />
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
       </div>
     </>
   )
