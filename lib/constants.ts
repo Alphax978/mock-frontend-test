381c84bb4737fc03ca872587661abe189779de3e
Alphax978 <jamesrawal9191@gmail.com>
2025-01-24 16:15:10 +0545

    product-details-page

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const app_name = process.env.NEXT_PUBLIC_APP_NAME || "nepzone2"
 export const app_slogan = process.env.NEXT_PUBLIC_APP_SLOGAN || "spend less, enjoy more"
 export const app_description = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "shop with no regrets"
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
